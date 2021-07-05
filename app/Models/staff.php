0b9a3d448f5bb16dec3014e0ec7e0da3f5917336
Cedric Matthew Verdida <chryhesmicazumic@gmail.com>
2021-07-05 21:49:16 +0800

    Backend

---
+++ app/Models/staff.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class staff extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'id ',
+        'staff_fname',
+        'staff_mname',
+        'staff_lname',
+        'full_address',
+        'telephone_number',
+        'date_of_birth',
+        'sex',
+        'nin'
+    ];
+}
